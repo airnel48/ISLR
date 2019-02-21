d26874bfcc00e8096173a37cc01f1368f25af073
airnel48 <nelson24@indiana.edu>
2019-02-20 20:42:06 -0500

    Add files via upload

--- Ch7.R
+++ Ch7.R
@@ -57,3 +57,92 @@ matlines(age.grid,se.bands,lwd=1,col="blue",lty=3)
 table(cut(age,4))
 fit=lm(wage~cut(age,4),data=Wage)
 coef(summary(fit))
+
+#Splines
+#bs() generates the entire matrix of basis fxn for splines with specified set of knots. 
+#default is cubic splines
+
+library(splines)
+fit = lm(wage~bs(age,knots=c(25,40,60)),data=Wage)
+pred=predict(fit,newdata=list(age=age.grid),se=T)
+plot(age,wage,col="gray")
+lines(age.grid,pred$fit,lwd=2)
+lines(age.grid,pred$fit+2*pred$se,lty="dashed")
+lines(age.grid,pred$fit-2*pred$se,lty="dashed")
+#can use df to produce a spline with knots at uniform quantiles of the data
+dim(bs(age,knots=c(25,40,60)))
+dim(bs(age,df=6))
+attr(bs(age,df=6),"knots")
+
+#fit a natural spline with 4 degrees of freedom
+fit2=lm(wage~ns(age,df=4),data=Wage)
+pred2=predict(fit2,newdata=list(age=age.grid),se=T)
+lines(age.grid,pred2$fit,col="red",lwd=2)
+
+#we can use smooth.spline() to produce a smooth spline
+plot(age,wage,xlim=agelims,cex=.5,col="darkgrey")
+title("Smoothing spline")
+fit=smooth.spline(age,wage,df=16)
+fit2=smooth.spline(age,wage,cv=TRUE)
+fit2$df
+lines(fit,col="red",lwd=2)
+lines(fit2,col="blue",lwd=2)
+legend("topright",legend=c("16 DF","6.8 DF"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
+
+#perform local regression
+plot(age,wage,xlim=agelims,cex=.5,col="darkgrey")
+title("Local Regression")
+fit=loess(wage~age,span=.2,data=Wage)
+fit2=loess(wage~age,span=.5,data=Wage)
+lines(age.grid,predict(fit,data.frame(age=age.grid)),col="red",lwd=2)
+lines(age.grid,predict(fit2,data.frame(age=age.grid)),col="blue",lwd=2)
+legend("topright",legend=c("Span=0.2","Span=0.5"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
+
+#GAMS
+#predict wage using natural spline functions of year and age, treating education as qualitative predictor
+#this is a large linear regression model using basis fxns
+gam1=lm(wage~ns(year,4)+ns(age,5)+education,data=Wage)
+
+#now using gam library
+#s() indicates we would like to use a smoothing spline
+install.packages("gam")
+library(gam)
+gam.m3=gam(wage~s(year,4)+s(age,5)+education,data=Wage)
+par(mfrow=c(1,3))
+plot(gam.m3,se=TRUE,col="blue")
+plot.gam(gam1,se=TRUE,col="red")
+
+#apply to anova to determine which model is best:
+#1 a GAM that excludes year
+#2 a GAM that uses a linear fxn of year
+#3 a GAM that uses a spline fxn of year
+
+gam.m1=gam(wage~s(age,5)+education,data=Wage)
+gam.m2=gam(wage~year+s(age,5)+education,data=Wage)
+anova(gam.m1,gam.m2,gam.m3,test="F")
+
+summary(gam.m2)
+
+preds=predict(gam.m2,newdata=Wage)
+
+#use local regression fits as buildign blocks in a GAM using the lo() fxn
+gam.lo=gam(wage~s(year,df=4)+lo(age,span=0.7)+education,data=Wage)
+plot.gam(gam.lo,se=TRUE,col="green")
+
+#use lo() to create intreactions before calling gam() fxn
+#here the first term is an interaction between year and age
+gam.lo.i=gam(wage~lo(year,age,span=0.5)+education,data=Wage)
+install.packages("akima")
+library(akima)
+plot(gam.lo.i)
+
+#we can fit a logisitc regression GAM with I() fxn to create binary response variable
+gam.lr=gam(I(wage>250)~year+s(age,df=5)+education,family=binomial,data=Wage)
+par(mfrow=c(1,3))
+plot(gam.lr,se=T,col="green")
+table(education,I(wage>250))
+
+#since no high earners in <HS category, we fit logistic GAM using all but this category
+gam.lr.s=gam(I(wage>250)~year+s(age,df=5)+education,family=binomial,data=Wage,subset=(education!="1. < HS Grad"))
+plot(gam.lr.s,se=T,col="green")
+#this produces more sensible results
